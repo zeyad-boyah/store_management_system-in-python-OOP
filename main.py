30b9064b36a9655a109633cb34cdcac39a244574
Zeyad-Elgohary <zeyadelgohary.ze@gmail.com>
2025-01-26 09:25:36 +0200

    add a staticmethod for trail

--- main.py
+++ main.py
@@ -1,44 +1,56 @@
 import csv
 
-class Item:
-    pay_rate = 0.8 # discount rate of 20%
-    all = [] # this list will contain every generated item
 
-    def __init__(self, name: str, price: float , quantity = 0):
-        # validations 
+class Item:
+    pay_rate = 0.8  # discount rate of 20%
+    all = []  # this list will contain every generated item
+
+    def __init__(self, name: str, price: float, quantity=0):
+        # validations
         assert price >= 0, f"the price {price} is not bigger than or equal to zero"
-        assert quantity >= 0, f"the quantity {quantity} is not bigger than or equal to zero"
- 
+        assert (
+            quantity >= 0
+        ), f"the quantity {quantity} is not bigger than or equal to zero"
+
         # assignment
         self.name = name
         self.price = price
         self.quantity = quantity
 
         # actions
-        Item.all.append(self) 
-    
-    def calculate_total_price (self):
+        Item.all.append(self)
+
+    def calculate_total_price(self):
         return self.price * self.quantity
-    
+
     @classmethod
     def instantiate_from_csv(cls):
-        with open('store_data.csv', 'r') as f:
+        with open("store_data.csv", "r") as f:
             reader = csv.DictReader(f)
             items = list(reader)
-        
-        # initiate each item from the file to the class 
+
+        # initiate each item from the file to the class
         for item in items:
             Item(
-                name= item["name"],
+                name=item["name"],
                 price=float(item["price"]),
-                quantity= int(item["quantity"])     
+                quantity=int(item["quantity"]),
             )
 
-    
+    @staticmethod
+    def is_integer(num):
+
+        if isinstance(num, float):
+            # this will only return true if the first decimal is only 0 ex: 5.0
+            return num.is_integer()
+        elif isinstance(num, int):
+            return True
+        else:
+            return False
+
     def __repr__(self):
-        return F"Item '{self.name}', '{self.price}', '{self.quantity}'."
-    
+        return f"Item '{self.name}', '{self.price}', '{self.quantity}'."
+
 
 Item.instantiate_from_csv()
 print(Item.all)
-
